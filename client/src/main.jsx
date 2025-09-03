22cced78a61b777335f1b71ae1bc375a1ea8134a
samemad <alsamemad775@gmail.com>
2025-09-03 19:11:25 +0300

    Fix admin panel routing - remove HashRouter basename

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // Change thisimport App from "./App.jsx";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/interior-portfolio">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
